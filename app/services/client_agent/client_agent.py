14d12ec9a2a30b17c6aa6a7ae54993b5361b179a
Blue <azuldebenedetti@gmail.com>
2025-12-16 23:31:14 +0000

    Work on Client agent

--- app/services/client_agent/client_agent.py
+++ app/services/client_agent/client_agent.py
@@ -1,28 +1,58 @@
 from typing import Any, Dict, Optional
+from openai import OpenAI
+from pydantic import BaseModel
 from app.services.agent_base import AgentBase
+from app.services.prompts.prompts import prompt_dict
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class CategorizeResponse(BaseModel):
+    intent: str
+    
 
 class ClientAgent(AgentBase):
     def __init__(self, name: str, client: Any, user_id: str, chat_id: str, user_message: str):
-        super().__init__(name, client)
+        super().__init__(name = name, client = client)
 
         self.user_id = user_id
         self.chat_id = chat_id
         self.user_message = user_message
+        self.message_intent = self._categorize_message_intent(response_format=CategorizeResponse)
 
+    def receive_message(self, packet):
+        return super().receive_message(packet)
     
-    #call LLM with parameters for differnet model (more or less thinking) and response format
-    def _LLM_call(self, response_format: Optional[Dict] = None):
-        
-        response = self.client.chat.completions.parse(
+    def get_intent(self):
+        return self.message_intent
+
+
+    def _categorize_message_intent(self, response_format: Optional[Any] = None):
+        #make an LLM call to figure out what to do with the user message
+        response = self.client.responses.parse(
             model="gpt-5-nano",
-            messages=self.memory.compile_process_logs(self.task_id, self.prompt),
-            response_format=response_format
+            input=self.user_message,
+            instructions="Categorize the user message into ONE of the following intents: SOCIAL, EMERGENCY, TASK",
+            # response_format=response_format
         )
 
-        return response
+        print("user intent:")
+        print(response.output[1].content[0].text)
 
-    def _cathegorise_message(self):
+            
+        return response.output[1].content[0].text
+    
+        #reply to non urgent message
+    def _handle_social_message(self):
 
-        #make an LLM call to figure out what to do with the user message
-        return
+
+        response = self.client.responses.parse(
+            model="gpt-5-nano",
+            input=prompt_dict["front_facing_agent_social_prompt"]
+        )
+    
+client = OpenAI()
+
+client_agent = ClientAgent("Client_Agent", client, "user_id", chat_id="Chat_id", user_message="THIS IS AN EMERGENCY TEXT!")
