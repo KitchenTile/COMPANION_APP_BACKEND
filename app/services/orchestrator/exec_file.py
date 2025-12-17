ac70f19e0b95646f78f85eccb8c8f72ec6cf6a5b
Blue <azuldebenedetti@gmail.com>
2025-12-17 20:47:32 +0000

    Update exec_file.py

--- app/services/orchestrator/exec_file.py
+++ app/services/orchestrator/exec_file.py
@@ -1,4 +1,5 @@
 import json
+from openai import OpenAI
 import redis
 
 from app.services.orchestrator.memory import ConversationManager
@@ -6,6 +7,7 @@ from app.services.orchestrator.orchestrator_agent import OrchestratorAgent
 from app.services.tools import tool_definitions, tool_dict
 from app.services.prompts import prompt_dict
 
+client = OpenAI
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -19,7 +21,7 @@ while True:
     print(f"Received task: {packet['task_id']}")
     
     #initialise the agent
-    orchestrator = OrchestratorAgent("OrchestratorAgent", tool_definitions, tool_dict, prompt_dict["reasoning_agent_prompt"], packet["chat_id"])
+    orchestrator = OrchestratorAgent(name="OrchestratorAgent",client=client ,tool_definitions=tool_definitions, tool_dict=tool_dict, prompt=prompt_dict["reasoning_agent_prompt"], chat_id=packet["chat_id"])
 
     #call function to run the loop
     result = orchestrator.receive_message(packet)
