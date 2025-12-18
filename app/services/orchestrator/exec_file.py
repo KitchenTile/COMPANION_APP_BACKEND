fdb24dbdcb45e93f930e6c826e57e6c43989cac5
Blue <azuldebenedetti@gmail.com>
2025-12-18 19:17:06 +0000

    Update exec_file.py

--- app/services/orchestrator/exec_file.py
+++ app/services/orchestrator/exec_file.py
@@ -1,30 +1,40 @@
 import json
+from dotenv import load_dotenv
 from openai import OpenAI
 import redis
 
-from app.services.orchestrator.memory import ConversationManager
 from app.services.orchestrator.orchestrator_agent import OrchestratorAgent
 from app.services.tools import tool_definitions, tool_dict
-from app.services.prompts import prompt_dict
+from app.services.prompts.prompts import prompt_dict
 
-client = OpenAI
+load_dotenv()
+
+client = OpenAI()
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 while True:
+
+    print("exec file running")
     #get data from queue
     _, raw_data = r.blpop('orchestrator_queue') 
+    print(raw_data)
     
     #define packet to send the agent
     packet = json.loads(raw_data)
     print(f"Received task: {packet['task_id']}")
     
     #initialise the agent
-    orchestrator = OrchestratorAgent(name="OrchestratorAgent",client=client ,tool_definitions=tool_definitions, tool_dict=tool_dict, prompt=prompt_dict["reasoning_agent_prompt"], chat_id=packet["chat_id"])
+    orchestrator = OrchestratorAgent(name="OrchestratorAgent",client=client ,tool_definitions=tool_definitions, tool_dict=tool_dict, prompt=prompt_dict["reasoning_agent_prompt"], user_id=packet["user_id"], chat_id=packet["chat_id"])
 
+    print("---")
+    print(orchestrator)
+    print("---")
     #call function to run the loop
     result = orchestrator.receive_message(packet)
 
+    result_dump = json.dumps(result)
+
     #oublish it
-    r.publish(f"chat_updates_{packet['chat_id']}", result)
+    r.publish(f"chat_updates_{packet['chat_id']}", result_dump)
