02b1e2a589de342d43f422404bf8afd6b3189804
Blue <azuldebenedetti@gmail.com>
2026-03-11 14:31:47 +0000

    Update exec_file.py

--- app/services/orchestrator/exec_file.py
+++ app/services/orchestrator/exec_file.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import redis
 
+from app.services.anticip8.anticip8_test import Anticip8RoutePredictor
 from app.services.orchestrator.orchestrator_agent import OrchestratorAgent
 from app.services.tools import tool_definitions, tool_dict
 from app.services.prompts.prompts import prompt_dict
+from app.utils.journey_planner import JourneyPlanner
 
 load_dotenv()
 
@@ -27,9 +29,14 @@ while True:
     #define packet to send the agent
     packet = json.loads(raw_data)
     print(f"Received task: {packet['task_id']}")
+
+    anticip8 = Anticip8RoutePredictor()
+
+
+    journey_planner = JourneyPlanner(tools = None, anticip8 = anticip8)
     
     #initialise the agent
-    orchestrator = OrchestratorAgent(name="OrchestratorAgent",client=client ,tool_definitions=tool_definitions, tool_dict=tool_dict, prompt=prompt_dict["reasoning_agent_prompt"], user_id=packet["user_id"], chat_id=packet["chat_id"])
+    orchestrator = OrchestratorAgent(name="OrchestratorAgent",client=client ,tool_definitions=tool_definitions, tool_dict=tool_dict, prompt=prompt_dict["reasoning_agent_prompt"], user_id=packet["user_id"], chat_id=packet["chat_id"], journey_planner = journey_planner)
 
     #call function to run the loop
     result = orchestrator.receive_message(packet)
