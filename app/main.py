29d8015faa3ce7e1128228082981c9796bfd8d9e
Blue <azuldebenedetti@gmail.com>
2026-03-18 21:57:44 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -145,7 +145,7 @@ async def async_route_calculation(request_data: DemoRequest):
 
     ws_payload = {
         "type": "ANTICIP8_RESULT",
-        "graph": journey_graph
+        "data": journey_graph
     }
 
     await websocket_manager.send_message(
